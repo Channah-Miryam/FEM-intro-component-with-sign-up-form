951f2783035f1bc43c1f6aab61ee695a9549e533
Channah <code.challenge.cv@gmail.com>
2022-09-08 23:14:50 +0100

    css improved

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-import 'core-js/es6/promise';
-import 'core-js/es6/set';
-import 'core-js/es6/map';
-import * as yup from 'yup';
+let firstName = document.getElementById('firstName');
+let lastName = document.getElementById('lastName');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
 
-let schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  email: yup.string().email()
-});
+button.addEventListener('click', validate());
 
-
-          <input type="text" placeholder="First Name" id="firstName" name="firstName" aria-label="first name">
-          <input type="text" placeholder="Last Name" id="lastName" name="lastName" aria-label="last name">
-          <input type="email" placeholder="Email address" id="email" name="email" aria-label="Email address">
-          <input type="password" placeholder="Password" id="password" name="password" aria-label="password"></input>
